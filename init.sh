05fe5e2e7366d3d40e7bbf85071b2b49808ff4e5
Hernanda Achmad <hernandaislearning@gmail.com>
2026-04-30 18:42:04 +0700

    add:furmark kdiskmark

--- init.sh
+++ init.sh
@@ -3,9 +3,17 @@ set -euo pipefail
 
 export DEBIAN_FRONTEND=noninteractive
 
+# Original invoking user
+export REAL_USER="${SUDO_USER:-$USER}"
+
+echo "[INFO] Running as real user: ${REAL_USER}"
+
 echo "[INFO] Update & install Ansible"
 sudo apt update -y
 sudo apt install -y ansible git
 
 echo "[INFO] Run Ansible playbook"
-ansible-playbook -i inventory.ini omoda-its_autoware_style.yaml
+ansible-playbook \
+  -i inventory.ini \
+  omoda-its_autoware_style.yaml \
+  --extra-vars "ansible_user_id=${REAL_USER}"
